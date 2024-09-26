53b0cd6ebe41ee7b7a5f460f3992f30af1590204
Omer Salih Pakdil <omerp@mobiliz.com.tr>
2024-09-26 15:57:05 +0300

    upload project

---
+++ components/EmojiSticker.js
@@ -0,0 +1,65 @@
+import { View, Image } from 'react-native';
+import { Gesture, GestureDetector } from 'react-native-gesture-handler';
+import Animated, { useAnimatedStyle, useSharedValue, withSpring } from 'react-native-reanimated';
+
+
+export default function EmojiSticker({ imageSize, stickerSource }) {
+
+
+  const scaleImage = useSharedValue(imageSize);
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
+
+  
+  const imageStyle = useAnimatedStyle(() => {
+    return {
+      width: withSpring(scaleImage.value),
+      height: withSpring(scaleImage.value),
+    };
+  });
+
+  const containerStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: translateX.value,
+        },
+        {
+          translateY: translateY.value,
+        },
+      ],
+    };
+  });
+  
+
+  const doubleTap = Gesture.Tap()
+  .numberOfTaps(2)
+  .onStart(() => {
+    if (scaleImage.value !== imageSize * 2) {
+      scaleImage.value = scaleImage.value * 2;
+    }
+  });
+
+  const drag = Gesture.Pan()
+  .onChange((event) => {
+    translateX.value += event.changeX;
+    translateY.value += event.changeY;
+  });
+
+
+  return (
+    <GestureDetector gesture={drag}>
+        <Animated.View style={[containerStyle,{ top: -400, right: -270 }]}>
+            <GestureDetector gesture={doubleTap}>
+                <Animated.Image
+                    source={stickerSource}
+                    resizeMode="contain"
+                    style={[imageStyle,{ width: imageSize, height: imageSize }]}
+                />
+            </GestureDetector>
+        </Animated.View>
+    </GestureDetector>
+  );
+}
+
+
